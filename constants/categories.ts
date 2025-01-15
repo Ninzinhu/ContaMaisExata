a6e576d30a558a9b592f1b1454c50fd2fb236b84
João Victor Emmanuell <pel118201@gmail.com>
2025-01-15 00:15:45 -0300

    🎨 :  Adding clean code

--- constants/categories.ts
+++ constants/categories.ts
@@ -1,41 +1,41 @@
 export const expenseCategories = [
-  'Comidas & Bebidas',
-  'Compras',
-  'Transporte',
-  'Casa',
-  'Entreterimento',
-  'Assistência médica',
-  'Educação',
-  'Utilidades',
-  'Viagem',
-  'Seguro',
-  'Cuidados Pessoais',
-  'Presentes',
-  'Investimentos',
-  'Outros'
+  "Comidas & Bebidas",
+  "Compras",
+  "Transporte",
+  "Casa",
+  "Entreterimento",
+  "Assistência médica",
+  "Educação",
+  "Utilidades",
+  "Viagem",
+  "Seguro",
+  "Cuidados Pessoais",
+  "Presentes",
+  "Investimentos",
+  "Outros",
 ];
 
 export const incomeCategories = [
-  'Salário',
-  'Negócios',
-  'Investimentos',
-  'Freelance',
-  'Presentes',
-  'Aluguel',
-  'Reembolsos',
-  'Outros'
+  "Salário",
+  "Negócios",
+  "Investimentos",
+  "Freelance",
+  "Presentes",
+  "Aluguel",
+  "Reembolsos",
+  "Outros",
 ];
 
 //Função auxiliar para obter categorias com base no tipo de transação
-export const getCategoriesByType = (type: 'renda' | 'despesa' | 'tudo') => {
+export const getCategoriesByType = (type: "renda" | "despesa" | "tudo") => {
   switch (type) {
-    case 'renda':
+    case "renda":
       return incomeCategories;
-    case 'despesa':
+    case "despesa":
       return expenseCategories;
-    case 'tudo':
+    case "tudo":
       return [...new Set([...incomeCategories, ...expenseCategories])];
     default:
       return [];
   }
-}; 
+};
